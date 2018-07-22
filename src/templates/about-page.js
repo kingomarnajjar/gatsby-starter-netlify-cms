e47e4d04ee8cd20f2ea74327c242a6de9a0e49bc
YOUR NAME <YOUR@EMAIL.com>
2018-07-22 20:45:22 +1000

    changed componentname

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Content, { HTMLContent } from '../blogComponents/Content'
+import Content, { HTMLContent } from '../components/Content'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Features from '../blogComponents/Features'
-import Testimonials from '../blogComponents/Testimonials'
-import Pricing from '../blogComponents/Pricing'
+import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
+import Pricing from '../components/Pricing'
 
 export const ProductPageTemplate = ({
   image,
